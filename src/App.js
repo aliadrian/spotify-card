743ff1172dc634ebb971885954685f50c837eddc
Adrian Nasrat <adriannasrat@gmail.com>
2025-02-18 15:12:13 +0100

    added a TODO

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
 
   return (
     <div className="App bg-[#222] grid place-content-center h-[100dvh]">
+      {/* TODO - change height and remove margin-top from clickFunction.js when a song is not playing */}
       <div className="w-[300px] h-[396px] hover:cursor-pointer">
         <ClickableCard />
       </div>
